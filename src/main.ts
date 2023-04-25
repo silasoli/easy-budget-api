1765e248c1cb290b72a5f13401a6bbd96504d417
Silasoli <silas.oliveira@blockhub.builders>
2023-04-25 02:28:03 -0300

    fix: try fix swagger error

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,14 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
+  });
 
   const port = configService.get('PORT');
   if (!port) throw new Error("Application port wasn't found");
