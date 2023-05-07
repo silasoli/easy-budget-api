0657e489b8001aedce6e18ad634553a6cf13db0a
Silasoli <silas.oliveira@blockhub.builders>
2023-05-07 13:44:47 -0300

    feat: adding route to list categories in front and translate categorie in create or update

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,12 +3,13 @@ import { ProductsController } from './controllers/products.controller';
 import { ProductsService } from './services/products.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Product, ProductSchema } from './schemas/product.entity';
+import { MaterialCategoriesController } from './controllers/products-categories.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
   ],
-  controllers: [ProductsController],
+  controllers: [ProductsController, MaterialCategoriesController],
   providers: [ProductsService],
 })
 export class ProductsModule {}
