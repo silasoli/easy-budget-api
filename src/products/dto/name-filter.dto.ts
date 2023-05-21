8fa92e2482cef3f2f525fe4e923d9f04fc522465
Silasoli <silas.oliveira@blockhub.builders>
2023-05-21 02:07:26 -0300

    fix: add optional filed

--- src/products/dto/name-filter.dto.ts
+++ src/products/dto/name-filter.dto.ts
@@ -1,7 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { CategoryFilterDto } from './category-filter.dto';
+import { IsString } from 'class-validator';
 
 export class NameFilterDto extends CategoryFilterDto {
   @ApiProperty({ required: false })
+  @IsString()
   name: string | null;
 }
